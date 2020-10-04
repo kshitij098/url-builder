aec154b0a05ff25cbd5954cf2c52063af5b9a4a8
Kshitij Tiwari <kshitiztewari098@gmail.com>
2020-10-04 13:54:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var text;
+const link = document.getElementById('link')
+function url_generator(){
+text='';
+var a=document.getElementById('URL').value;
+var b=document.getElementById('source').value;
+var c=document.getElementById('team').value;
+text+=a+'#src='+b+'&team='+c;
+if(pattern[a]!=".+\.com")
+{
+	alert("Hello! I am an alert box!!");
+}
+link.value=text;
+event.preventDefault();
+}
+document.getElementById('press').addEventListener('click',url_generator);
+function Copy(){
 
+link.select();
+document.execCommand("Copy");
+alert("Copied URL:"+link.value);
+}
